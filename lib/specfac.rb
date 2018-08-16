0e19862d1d8ca57da14ad6279cd9eb9a9315b340
viktharien <viktharien@zoho.com>
2018-08-16 15:27:12 -0500

    write generate method : cyuhcf

--- lib/specfac.rb
+++ lib/specfac.rb
@@ -2,6 +2,9 @@ require 'specfac'
 require 'thor'
 module SpecFac
   class CLI < Thor
-    # ...
+    desc "generate [actions]", "generates tests for specified actions"
+    def generate(actions)
+      
+    end
   end
 end
