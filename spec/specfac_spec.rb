faca247e0f135955635284cbdaef78a51498aef8
viktharien <viktharien@zoho.com>
2018-08-22 08:50:58 -0500

    fix dir

--- spec/specfac_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Specfac do
-  it "has a version number" do
-    expect(Specfac::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
